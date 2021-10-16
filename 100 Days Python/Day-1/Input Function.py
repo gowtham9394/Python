0d350a9699dcf95d487ba983ee651a0ba8073d85
Gowtham <gowtham9394@gmail.com>
2021-10-16 06:46:16 +0000

    Day -1 commits

---
+++ 100 Days Python/Day-1/Input Function.py
@@ -0,0 +1,6 @@
+input("what is your name.?")
+
+print("Hello " + input ("what is your username"))
+
+print(len(input("prints the length of input string: ")))
+
---
+++ 100 Days Python/Day-1/Variables.py
@@ -0,0 +1,16 @@
+name = input("what is your name ")
+length = len(name)
+print(name + " and the lenght of the name = " + str(length))
+
+
+#program that switches the values stored in the variables a and b.
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print(a, b)
+
